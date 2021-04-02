a072d34e501e6a1f4302c6d45ac7b445f1a105bb
Pavel Novak <xnovak@fi.muni.cz>
2021-04-02 11:46:01 +0200

    removed spaces

--- src/biblio.rs
+++ src/biblio.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 pub fn find_biblio(text : &String) -> HashMap<String, String>{
     let mut biblio = HashMap::<String, String>::new();
     let mut results = HashSet::<String>::new();
-    let regexBiblio = Regex::new(r"\d{1,2}\.{0,1}\s+Bibliography\s+[^\.](?s:.)*").unwrap();
+    let regexBiblio = Regex::new(r"\d{1,2}\.{0,1}\s+Bibliography\s+[^\.\n](?s:.)*").unwrap();
     let regexLiterature = Regex::new(r"\d{1,2}\.{0,1}\s+Literature\s+[^\.](?s:.)*").unwrap();
     let regexEntry = Regex::new(r"\[[^\]]+\]\s+([^\[\n]+\n)+").unwrap();
 
@@ -25,13 +25,18 @@ pub fn find_biblio(text : &String) -> HashMap<String, String>{
         let regexCaptures = Regex::new(r"(\[[^\]]+\])\s+(([^\[\n]+\n)+)").unwrap();
         for name in regexEntry.find_iter(&i) {
             let caps = regexCaptures.captures(name.as_str()).unwrap();
-            println!("{} {}", String::from(caps.get(1).unwrap().as_str()), String::from(caps.get(2).unwrap().as_str()));
-            biblio.insert(
-                String::from(caps.get(1).unwrap().as_str()), 
-                String::from(caps.get(2).unwrap().as_str())
-            );
+            let name = remove_whitespaces(String::from(caps.get(1).unwrap().as_str()));
+            let content = remove_whitespaces(String::from(caps.get(2).unwrap().as_str()));
+            println!("{} {}\n", name.to_string(), content.to_string());
+            biblio.insert( name.to_string(), content.to_string());
         }
     }
     biblio
+}
+
+fn remove_whitespaces(text: String) -> String {
+    let re = Regex::new(r"\s+").unwrap();
+    let t = re.replace_all(&text, " ").to_string();
+    t
 }
 
