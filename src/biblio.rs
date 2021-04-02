e843cdeef7a3fdb46b9459e73b034eacb83bf552
Pavel Novak <xnovak@fi.muni.cz>
2021-04-02 11:12:17 +0200

    Bibliography select patch

--- src/biblio.rs
+++ src/biblio.rs
@@ -4,8 +4,8 @@ use regex::Regex;
 pub fn find_biblio(text : &String) -> HashMap<String, String>{
     let mut biblio = HashMap::<String, String>::new();
     let mut results = HashSet::<String>::new();
-    let regexBiblio = Regex::new(r"\d{1,2}\.{0,1}\s+Bibliography\s+(?s:.)*").unwrap();
-    let regexLiterature = Regex::new(r"\d{1,2}\.{0,1}\s+Literature\s+(?s:.)*").unwrap();
+    let regexBiblio = Regex::new(r"\d{1,2}\.{0,1}\s+Bibliography\s+[^\.](?s:.)*").unwrap();
+    let regexLiterature = Regex::new(r"\d{1,2}\.{0,1}\s+Literature\s+[^\.](?s:.)*").unwrap();
     let regexEntry = Regex::new(r"\[[^\]]+\]\s+([^\[\n]+\n)+").unwrap();
 
     if regexBiblio.is_match(&text) {
@@ -25,6 +25,7 @@ pub fn find_biblio(text : &String) -> HashMap<String, String>{
         let regexCaptures = Regex::new(r"(\[[^\]]+\])\s+(([^\[\n]+\n)+)").unwrap();
         for name in regexEntry.find_iter(&i) {
             let caps = regexCaptures.captures(name.as_str()).unwrap();
+            println!("{} {}", String::from(caps.get(1).unwrap().as_str()), String::from(caps.get(2).unwrap().as_str()));
             biblio.insert(
                 String::from(caps.get(1).unwrap().as_str()), 
                 String::from(caps.get(2).unwrap().as_str())
