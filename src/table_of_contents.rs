18fb47e5efeb4fb185929b7644fe97dd0a850de8
AnhMinh <anhminh@seznam.cz>
2021-04-27 12:22:20 +0200

    Pridani testu pro table_of_contents, pridani pojmenovani pro capture groups.

--- src/table_of_contents.rs
+++ src/table_of_contents.rs
@@ -9,20 +9,16 @@ const CERT_MAX_PAGE: i32 = 200;
 pub fn find_table_of_content(text: &String) -> Vec<LineOfContents> {
     let mut res: Vec<LineOfContents> = Vec::new();
 
-    let table_section_regex =
-        Regex::new(r"(?m)(Table of Contents$|TABLE OF CONTENTS|Contents$|Content$|INDEX$|CONTENTS:$)\n([^\n]*\n){1,150}")
-            .unwrap();
-    // this regex works on base cases
-    // (\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*([A-Z](\w|\s|[“”\-\(\)\-:,/]|\w\.)*)(\s|\.)+(\d+)
+    let mut table_section = find_section(text);
+
     let simple_line_regex =
-        Regex::new(r"(\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*([A-Z](\w|\s|[“”\-\(\)\-:,/]|\w\.)*)(\s|\.)+(\d+)")
+        Regex::new(r"(?P<section>\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*(?P<title>[A-Z](\w|\s|[“”\-\(\)\-:,/]|\w\.)*)(\s|\.)+(?P<page>\d+)")
             .unwrap();
     let dots_regex = Regex::new(r"(\.|\.\s){10,}").unwrap();
     let no_dots_line_regex =
-        Regex::new(r"(\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*([A-Z]((\w|[“”\-\(\)\-:,/]|\w\.)+\s?)+)(\s|\.)+(\d+)")
+        Regex::new(r"(?P<section>\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*(?P<title>[A-Z]((\w|[“”\-\(\)\-:,/]|\w\.)+\s?)+)(\s|\.)+(?P<page>\d+)")
             .unwrap();
 
-    let mut table_section = find_section(text, table_section_regex);
     let mut section;
 
     // println!("Section head: {}", table_section);
@@ -53,25 +49,28 @@ fn parse_lines(res: &mut Vec<LineOfContents>, regex: Regex, section: &mut Vec<St
     }
 }
 
-fn find_section(text: &String, table_regex: regex::Regex) -> String {
+fn find_section(text: &String) -> String {
     let mut text_clone = text.clone();
+    let table_section_regex =
+        Regex::new(r"(?m)(Table of Contents$|TABLE OF CONTENTS|Contents$|Content$|INDEX$|CONTENTS:$)\n([^\n]*\n){1,150}")
+            .unwrap();
 
     erase_wrong_beginning(&mut text_clone);
 
-    let mut head = table_regex.find(&text_clone).unwrap();
+    let mut head = table_section_regex.find(&text_clone).unwrap();
 
     let second_wrong_header_regex = Regex::new(r"Content\s+Manager").unwrap();
     if second_wrong_header_regex.is_match(head.as_str()) {
         let offset = second_wrong_header_regex.find(&*text_clone).unwrap().end();
         crop_letters(&mut text_clone, offset);
-        head = table_regex.find(&text_clone).unwrap();
+        head = table_section_regex.find(&text_clone).unwrap();
     }
 
     let third_wrong_header_regex = Regex::new(r"Default\sPersonalisation\sContent|Content\s+Tab").unwrap();
     while third_wrong_header_regex.is_match(head.as_str()) {
         let offset = third_wrong_header_regex.find(&*text_clone).unwrap().end();
         crop_letters(&mut text_clone, offset);
-        head = table_regex.find(&text_clone).unwrap();
+        head = table_section_regex.find(&text_clone).unwrap();
     }
 
     head.as_str().to_string()
@@ -122,17 +121,17 @@ fn extract_line_info(line: &String, regex: regex::Regex, last_page: i32) -> Line
     let mut result = LineOfContents::new();
     let caps = regex.captures(line).unwrap();
 
-    let mut section_number = caps.get(1).unwrap().as_str().to_string();
+    let mut section_number = caps.name("section").unwrap().as_str().to_string();
     if section_number.chars().last().unwrap().eq(&'.') {
         section_number.pop();
     }
 
-    let mut section_title = caps.get(3).unwrap().as_str().to_string();
+    let mut section_title = caps.name("title").unwrap().as_str().to_string();
     while section_title.chars().last().unwrap().eq(&' ') {
         section_title.pop();
     }
     //this is not safe, should be OK/Err options
-    let page = caps.get(6).unwrap().as_str().parse::<i32>().unwrap();
+    let page = caps.name("page").unwrap().as_str().parse::<i32>().unwrap();
 
 
     if section_title.len() > CHAPTER_MAX_CHAR || section_title.len() < CHAPTER_MIN_CHAR {
@@ -157,37 +156,95 @@ fn extract_line_info(line: &String, regex: regex::Regex, last_page: i32) -> Line
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn simplest_case_test() {
-        let simplest_case: String = String::from(
-            "Contents
-A. Certification......................................................................................................................6
-   1.   Preliminary Remarks....................................................................................................6
-   2.   Specifications of the Certification Procedure...............................................................6
-   3.   Recognition Agreements..............................................................................................7
-   4.   Performance of Evaluation and Certification................................................................8
-   5.   Validity of the Certification Result.................................................................................8
-   6.   Publication....................................................................................................................9
-B. Certification Results.......................................................................................................10
-   1. Executive Summary....................................................................................................11
-   2. Identification of the TOE.............................................................................................12
-   3. Security Policy............................................................................................................15
-   4. Assumptions and Clarification of Scope.....................................................................15
-   5. Architectural Information.............................................................................................16
-   6. Documentation...........................................................................................................16
-   7. IT Product Testing.......................................................................................................16
-   8. Evaluated Configuration.............................................................................................17
-   9. Results of the Evaluation............................................................................................18
-   10. Obligations and Notes for the Usage of the TOE.....................................................19
-   11. Security Target..........................................................................................................20
-   12. Regulation specific aspects (eIDAS, QES)..............................................................20
-   13. Definitions.................................................................................................................20
-   14. Bibliography..............................................................................................................23
-C. Excerpts from the Criteria..............................................................................................26
-
-D. Annexes.........................................................................................................................27"
-        );
-        find_table_of_content(&simplest_case);
+    fn simplest_table_test() {
+        let simplest_table_case = String::from("Here I have some text that is before the table of contents \
+        Blablablabla
+        Tydyda
+        Contents
+               1. The Very First Section .............. 21
+               2. The Second Section .................. 22
+               3. The Third Section ....................23
+               ");
+        assert_eq!(find_section(&simplest_table_case),
+        String::from("Contents
+               1. The Very First Section .............. 21
+               2. The Second Section .................. 22
+               3. The Third Section ....................23\n"));
     }
+    #[test]
+    fn wrong_header_table_test() {
+        let wrong_header_table_case: String = String::from("Here I have some text that is before the table of contents\n
+                Document information
+        Info                 Content
+        Keywords            CC, Security Target Lite, P60D024/016/012yVB(Y/Z/A)/yVF
+                Blablablabla
+                Tydyda
+                Contents
+                       1. The Very First Section .............. 21
+                       2. The Second Section .................. 22
+                       3. The Third Section ....................23
+                       ");
+        assert_eq!(find_section(&wrong_header_table_case),
+                   String::from("Contents
+                       1. The Very First Section .............. 21
+                       2. The Second Section .................. 22
+                       3. The Third Section ....................23\n"));
+    }
+    #[test]
+    fn wrong_middle_table_test() {
+        let wrong_middle_table_case: String = String::from("Here I have some text that is before the table of contents\n
+        Document information
+Info                 Content
+Keywords            CC, Security Target Lite, P60D024/016/012yVB(Y/Z/A)/yVF
+        Blablablabla
+        Here I have some text in which the word Content occur.
+        Also, it can occur at the end of line like Content
+        Tab 1.3 or
+        Content
+        Manager
+        Really terrible.
+        Tydyda
+        Contents
+               1. The Very First Section .............. 21
+               2. The Second Section .................. 22
+               3. The Third Section ....................23
+               ");
+        assert_eq!(find_section(&wrong_middle_table_case),
+                   String::from("Contents
+               1. The Very First Section .............. 21
+               2. The Second Section .................. 22
+               3. The Third Section ....................23\n"));
+    }
+    #[test]
+    fn simple_line_test(){
+        let simple_line = String::from("1.1 The Very First Section .............. 21");
+        let simple_line_regex =
+            Regex::new(r"(\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*([A-Z](\w|\s|[“”\-\(\)\-:,/]|\w\.)*)(\s|\.)+(\d+)")
+                .unwrap();
+        let expected_section = String::from("1.1");
+        let expected_title = String::from("The Very First Section");
+        let expected_page = 21;
+        let result = extract_line_info(&simple_line, simple_line_regex , 0);
+        assert_eq!(result.section, expected_section);
+        assert_eq!(result.title, expected_title);
+        assert_eq!(result.page, expected_page);
+    }
+    #[test]
+    fn no_dots_line_test(){
+        let no_dots_line = String::from("1.1 The Very First Section             21");
+        let no_dots_line_regex =
+            Regex::new(r"(?P<section>\d{1,2}(\.\d)*|[A-Z]\.|\d{1,2}.)\s*(?P<title>[A-Z]((\w|[“”\-\(\)\-:,/]|\w\.)+\s?)+)(\s|\.)+(?P<page>\d+)")
+                .unwrap();
+        assert!(no_dots_line_regex.is_match(&no_dots_line));
+
+        let expected_section = String::from("1.1");
+        let expected_title = String::from("The Very First Section");
+        let expected_page = 21;
+        let result = extract_line_info(&no_dots_line, no_dots_line_regex , 0);
+        assert_eq!(result.section, expected_section);
+        assert_eq!(result.title, expected_title);
+        assert_eq!(result.page, expected_page);
+    }
+
 }
